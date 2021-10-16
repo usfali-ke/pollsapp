e830e763a7732694bfae2fd1be65a7a3c79b9ab8
usfali-ke <adanpare33@gmail.com>
2021-10-16 03:43:49 -0700

    view changed to generic

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
+
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-	path('', views.index, name='index'),
+
+	path('', views.IndexView.as_view(), name='index'),
 	# ex polls/4
-	path('<int:question_id>/', views.detail, name='detail'),
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 	# ex polls/4/results
-	path('<int:question_id>/results/', views.results, name='results'),
+	path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
 	# ex polls/4/votes
 	path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
